a813ca5227a035ca4f86b6d8f2d2e03036a9e4b4
urikialy <uri.kialy@msmail.ariel.ac.il>
2024-06-16 14:49:40 +0300

    upload  profile

---
+++ q2/Kosaraju.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <functional> 
+#include <list>
+#include <deque>
+#include <algorithm>
+void KosarajuList();
+void Kosarajudeque();
---
+++ q2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include "Kosaraju.hpp"
+
+int main() {
+        //KosarajuList();
+        Kosarajudeque();
+    return 0;
+}
+
+
